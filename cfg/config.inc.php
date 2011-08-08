7cc5c6fe4f55ec66d99772b34d394211351a3ee2
Lochmatter Christian <christian.lochmatter@gmx.ch>
2011-08-08 20:19:11 +0200

    bugfix: missed comma added

--- cfg/config.inc.php
+++ cfg/config.inc.php
@@ -2,7 +2,7 @@
 $g_pathToPictures="T:\\1Geschaeftsdaten\\Internetseite_ETK";
 $g_pathToConvertProg="T:\\1Geschaeftsdaten\\Internetseite_ETK\\BilderVerkleinern\\lib\\xnview\\XnView\\nconvert.exe";
 $aBase = array (
-  $g_pathToPictures."\\doc\\hauptseite\\aktuell\\anlaesse"
+  $g_pathToPictures."\\doc\\hauptseite\\aktuell\\anlaesse",
   $g_pathToPictures."\\doc\\hauptseite\\aktuell\\news",
   $g_pathToPictures."\\hauptseite\\aktuell\\motto",
   $g_pathToPictures."\\doc\\hauptseite\\aktuell\\news",
