4d2f860dccffff20287e93c5475aae43fb6baa7d
Lochmatter Christian <christian.lochmatter@gmx.ch>
2011-08-08 15:39:06 +0200

    initial import

---
+++ bin/BilderVerkleinernn.bat
@@ -0,0 +1,5 @@
+@echo off
+SET PROG_PATH=Z:\LOC\kjoff\bin\doIt
+cd %PROG_PATH%
+%PROG_PATH%\lib\php\php.exe %PROG_PATH%\lib\doit.php %PROG_PATH%
+pause
---
+++ cfg/config.inc.php
@@ -0,0 +1,4 @@
+<?PHP
+$g_pathToPictures="Z:\\LOC\\kjoff";
+$g_pathToConvertProg="Z:\\LOC\\Program Files\\xnview\\XnView\\nconvert.exe";
+?>
